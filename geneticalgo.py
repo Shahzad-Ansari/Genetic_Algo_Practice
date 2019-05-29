0117f2cc7a8183a5954a024fc93a6e1cb3165e4e
Shahzad-Ansari <34826823+Shahzad-Ansari@users.noreply.github.com>
2019-05-29 13:09:52 -0500

    create function to crossover genes , non functional

--- geneticalgo.py
+++ geneticalgo.py
@@ -22,8 +22,39 @@ def selection(agentArray):
     agentArray.sort(key=lambda x: x.fitness , reverse = True)
     print(agentArray[0].string + " " + str(agentArray[0].fitness))
     print(agentArray[1].string + " " + str(agentArray[1].fitness))
+    breedingPairs = [agentArray[0] , agentArray[1]]
+    return breedingPairs
+def crossover(breedingPairs):
+    random_num = random.randint(0,length)
+    breedingPair1 = breedingPairs[0].string
+    breedingPair2 = breedingPairs[1].string
 
-    return [agentArray[0] + agentArray[1]]
+    crossover_array1 = []
+    crossover_array2 = []
+    crossover_array3 = []
+    crossover_array4 = []
+    count = 0
+    for i in range(length):
+        if count <= random_num:
+            crossover_array1.append(breedingPair1[count])
+            crossover_array2.append(breedingPair2[count])
+            count+=1
+        else:
+            crossover_array3.append(breedingPair1[count])
+            crossover_array4.append(breedingPair2[count])
+            count+=1
+
+
+    breededArray1 = crossover_array1 + crossover_array2
+    breededArray2 = crossover_array2 + crossover_array4
+
+    breedingPairs[0].string = breededArray1
+    breedingPairs[1].string = breededArray2
+    return breedingPairs
+
+
+
+
 
 
 
@@ -34,6 +65,8 @@ def main():
     agent_array = generate_population()
     fitness(agent_array,user_string)
     breeding_pairs = selection(agent_array)
+    breeding_pairs = crossover(breeding_pairs)
+    print(str(breeding_pairs[0].string) + " " + str(breeding_pairs[1].string))
 
 
 
--- venv/Agent.py
+++ venv/Agent.py
@@ -7,5 +7,9 @@ class Agent:
         self.legnth = legnth
         self.string = "".join(random.choice(string.ascii_letters) for i in range(legnth))
         self.fitness = -1
+
     def __str__(self):
         return "Random string is:" + str(self.string) + "with a fitness score of " + str(self.fitness)
+    def printString(self):
+        print( "Random string is:" + str(self.string) + "with a fitness score of " + str(self.fitness))
+
